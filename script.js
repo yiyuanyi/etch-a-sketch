30c1aaf1834ab01ca6b79e86633c080af3bff5b9
Matt H <59845559+yiyuanyi@users.noreply.github.com>
2020-09-05 11:28:02 +0100

    move js link to bottom of <body> in html so page loads before js file

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const container = document.querySelector('.container');
 
 function createGrid() {
-for (let i = 0; i <= 16; i++) {
+for (let i = 0; i < 16; i++) {
         let square = document.createElement('div');
         container.appendChild(square);
         square.setAttribute("class","square");
     };
 };
 
-window.addEventListener('onload', createGrid());
+window.addEventListener('onclick', createGrid());
